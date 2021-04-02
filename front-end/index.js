4cae48534f9230b033f64b41df2eb61a9c2574f9
Tanilba <71246801+Tanilba@users.noreply.github.com>
2021-04-02 16:51:29 +1100

    Add files via upload

---
+++ front-end/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Log_in from './Log_in';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Log_in />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
