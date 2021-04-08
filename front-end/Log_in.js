c25ae71e5398819f3f540ecfdb3001a824aef1dd
Tanilba <71246801+Tanilba@users.noreply.github.com>
2021-04-09 05:07:40 +1000

    Update Log_in.js

--- front-end/Log_in.js
+++ front-end/Log_in.js
@@ -4,11 +4,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Dashboard from "./Dashboard";
+import Dashboard from './Dashboard'
 import ReactDOM from "react-dom";
 
 
@@ -35,46 +37,6 @@ const useStyles = makeStyles((theme) => ({
 function Log_in() {
   const classes = useStyles();
 
-
-
-  async function getUser() {
-
-
-
-    const response = await fetch("http://localhost:3001/");
-    const json = await response.json();
-    console.log(json)
-
-
-    const username = json[0].username
-    const password = json[0].password
-
-    
-    //console.log(username)
-    //console.log(password)
-
-    let USR = document.forms["signInForm"]["email"].value;
-    let PWD = document.forms["signInForm"]["password"].value;
-
-    //if user and password inputted matches a db record then display dash (not finished)
-    if(USR === username && PWD === password)
-    {
-
-        dash()
-
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
   return (
       <Container component="main" maxWidth="xs">
         <CssBaseline />
@@ -83,14 +45,14 @@ function Log_in() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <form className={classes.form} name={"signInForm"} onClick={getUser}>
+          <form className={classes.form} name={"signInForm"} onSubmit={Validate}>
             <TextField
                 variant="outlined"
                 margin="normal"
                 required
                 fullWidth
                 id="email"
-                label="Username"
+                label="Email Address"
                 name="email"
                 autoComplete="email"
                 autoFocus
@@ -119,9 +81,18 @@ function Log_in() {
             >
               Sign In
             </Button>
-            <button onClick={getUser}>
-              TRY
-            </button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
           </form>
         </div>
         <Box mt={8}>
@@ -131,23 +102,25 @@ function Log_in() {
   );
 }
 
-function dash(){
 
-  ReactDOM.render(
-      <React.StrictMode>
-        <Dashboard />
-      </React.StrictMode>,
-      document.getElementById('root')
+function Validate()
+{
 
 
-  );
+  let EML = document.forms["signInForm"]["email"].value;
+  let PWD = document.forms["signInForm"]["password"].value;
 
+  if(EML === "admin" && PWD ==="admin")
+  {
+    ReactDOM.render(
+        <React.StrictMode>
+          <Dashboard />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+  }else
+  {
+    alert("incorrect login details");
+  }
 }
 
-
-
-
-
-
-
-export default Log_in;
