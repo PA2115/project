01e8cd6a9d4214b752647a0b793e2461362b9bfa
Tanilba <71246801+Tanilba@users.noreply.github.com>
2021-06-02 18:19:08 +1000

    Update Chart.js
    this uses Google React charts so make sure to have that package downloaded

--- front-end/Chart.js
+++ front-end/Chart.js
@@ -1,55 +1,138 @@
-import React from 'react';
 import { useTheme } from '@material-ui/core/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
-import Title from './Title';
+
+import React, {useEffect, useState} from "react";
+
+import Chart from "react-google-charts";
 
 
-// Generate Sales Data
-function createData(time, amount) {
-    return { time, amount };
-}
 
-const data = [
-    createData('00:00', 0),
-    createData('03:00', 300),
-    createData('06:00', 600),
-    createData('09:00', 800),
-    createData('12:00', 1500),
-    createData('15:00', 2000),
-    createData('18:00', 2400),
-    createData('21:00', 2400),
-    createData('24:00', undefined),
-];
+export default function CPA() {
 
-export default function Chart() {
-    const theme = useTheme();
+
+
+    const [params, getParams] = useState([]);
+
+    const grab = async() =>
+    {
+        try{
+            const response = await fetch("http://localhost:3001/calcCPA", {
+                method: "POST",
+            })
+            const jsonData = await response.json()
+
+            getParams(jsonData)
+
+        }catch(err){
+            console.error(err.message)
+        }
+
+    }
+
+    useEffect(() => {
+        grab();
+    }, []);
+
+    const chartData = [['offer name', 'cpa']]
+    for (let i = 0; i < params.length; i ++) {
+
+        chartData.push([params[i].offer_name, params[i].example_count])
+    }
 
     return (
-        <React.Fragment>
-            <Title>Overview - Offer Performance</Title>
-            <ResponsiveContainer>
-                <LineChart
-                    data={data}
-                    margin={{
-                        top: 16,
-                        right: 16,
-                        bottom: 0,
-                        left: 24,
-                    }}
-                >
-                    <XAxis dataKey="time" stroke={theme.palette.text.secondary} />
-                    <YAxis stroke={theme.palette.text.secondary}>
-                        <Label
-                            angle={270}
-                            position="left"
-                            style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
-                        >
-                            Sales ($)
-                        </Label>
-                    </YAxis>
-                    <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
-                </LineChart>
-            </ResponsiveContainer>
-        </React.Fragment>
+        <Chart
+            width={'500px'}
+            height={'300px'}
+            chartType="Bar"
+            loader={<div>Loading Chart</div>}
+            data={chartData}
+            options={{
+                // Material design options
+                chart: {
+                    title: 'Estimated CPA per offer',
+                },
+            }}
+            chartWrapperParams={{ view: { columns: [0,1] } }}
+            // controls={[
+            //     {
+            //         controlType: 'CategoryFilter',
+            //         options: {
+            //             filterColumnIndex: 2,
+            //             ui: {
+            //                 labelStacking: 'vertical',
+            //                 label: 'Filter by State:',
+            //                 allowTyping: false,
+            //                 allowMultiple: false,
+            //             },
+            //         },
+            //     },
+            // ]}
+        />
     );
 }
+
+function OfferReach() {
+
+
+    const [results, getResult] = useState([]);
+
+    const grab = async() =>
+    {
+        try{
+            const response = await fetch("http://localhost:3001/sharedTransactions", {
+                method: "POST",
+            })
+            const jsonData = await response.json()
+
+            getResult(jsonData)
+
+        }catch(err){
+            console.error(err.message)
+        }
+
+    }
+
+    useEffect(() => {
+        grab();
+    }, []);
+
+    const chartData = [['Reach', 'Suburb', 'State']]
+    for (let i = 0; i < results.length; i ++) {
+        chartData.push([results[i].suburb, results[i].reach, results[i].state])
+    }
+
+    return (
+        <Chart
+            width={'500px'}
+            height={'300px'}
+            chartType="Bar"
+            loader={<div>Loading Chart</div>}
+            data={chartData}
+            options={{
+                // Material design options
+                chart: {
+                    title: 'Overall Reach',
+                },
+            }}
+            chartWrapperParams={{ view: { columns: [0, 1] } }}
+            controls={[
+                {
+                    controlType: 'CategoryFilter',
+                    options: {
+                        filterColumnIndex: 2,
+                        ui: {
+                            //labelStacking: 'vertical',
+                            label: 'Filter by State:',
+                            allowTyping: false,
+                            allowMultiple: false,
+                        },
+                    },
+                },
+            ]}
+        />
+    );
+}
+
+export {
+    OfferReach,
+}
+
